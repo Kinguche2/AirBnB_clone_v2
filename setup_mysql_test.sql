5c4ec99b0a498136aa0574867f2ad83942c6e78b
Uche Kingsley Ogbonna <99538237+Kinguche2@users.noreply.github.com>
2022-09-14 04:49:39 +0100

    Update setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,12 @@
--- Write a script that prepares a MySQL server for the project:
+-- creating test user
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
-SET PASSWORD FOR 'hbnb_test'@'localhost' = PASSWORD('hbnb_test_pwd');
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+GRANT USAGE ON *.*
+      TO 'hbnb_test'@'localhost'
+      IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.*
+      TO 'hbnb_test'@'localhost'
+      IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT ON performance_schema.*
+      TO 'hbnb_test'@'localhost'
+      IDENTIFIED BY 'hbnb_test_pwd';
+FLUSH PRIVILEGES;
