2ebbf266d7b1f189f6728b7dcff642d2b6b27dd5
root <ucheogbonnak@gmail.com>
2022-10-16 14:14:06 -0700

    new

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -7,19 +7,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
-def home():
+def index():
     """
     returns 'Hello HBNB!'
     """
     return "Hello HBNB!"
 
+
 @app.route('/', strict_slashes=False)
 def hbnb():
     """
-    returns 'HBNB!'
+    returns 'HBNB'
     """
-    return "HBNB!"
+    return "HBNB"
 
 
 if __name__ == '__main__':
